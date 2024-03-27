58c440605ef20c3418a43fa1a1648a5a0feffaef
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2024-03-27 10:09:48 +0000

    updates

--- game.js
+++ game.js
@@ -10,10 +10,12 @@ function load(phaser, assets) {
                     frameHeight: 16
                 })
             case 'img':
-                console.log(`assets/${asset[0]}.png`)
+                //console.log(`assets/${asset[0]}.png`)
                 phaser.load.image(asset[0], `assets/${asset[0]}.png`)
             case 'map':
                 phaser.load.tilemapTiledJSON(asset[0], `assets/maps/${asset[0]}.json`)
+            case 'audio':
+                phaser.load.audio(asset[0], `assets/audio/${asset[0]}.mp3`)
         }
 
     });
@@ -43,21 +45,28 @@ function loadScene(scene) {
 }
 var hiding = false
 var attack = false
+var preattack=false
+var chldcol=false
 var win = 0
+var children = 0
+var fadeTime=0
+var pointer = [0, 0]
 var hour = [12, 0]
 var hattack = rand(50, 10)
 var emo = rand(5000, 1000)
 var player;
 var hKey;
-var children;
 var map;
 
 const main = {
     onStart: function () {
 
-        load(this, [['main', 'map'], ['citytiles', 'img'], ['ground', 'img'], ['mask', 'img'], ['emotes', 'img'], ['chars', 'img'], ['schoolassets', 'img'], ['player', 'ss']])
+        load(this, [['ambeince', 'audio'],['bully','audio'],['child','audio'], ['main', 'map'], ['citytiles', 'img'], ['ground', 'img'], ['mask', 'img'], ['heartEmote', 'img'], ['chars', 'img'], ['schoolassets', 'img'], ['player', 'ss']])
     },
     onCreate: function () {
+        var mus=this.sound.add('ambeince',{volume:.5})
+        mus.loop=true
+        mus.play()
         map = this.make.tilemap({ key: "main" });
         const sp = map.findObject("data", obj => obj.name === "Spawn Point");
         player = this.player = this.physics.add.sprite(sp.x, sp.y, 'player')
@@ -95,7 +104,7 @@ const main = {
         var objs = map.createLayer("Objects", ['citytiles', 'schoolassets', 'ground', 'chars'], 0, 0);
         var coll = map.createLayer("Collision", ['citytiles', 'schoolassets', 'ground', 'chars'], 0, 0);
         var cl = map.createLayer("cleanup", ['citytiles', 'schoolassets', 'ground', 'chars'], 0, 0);
-        var cl2 = map.createLayer("cleanup2", ['citytiles', 'schoolassets', 'ground', 'chars'], 0, 0);
+        var cl2 = this.cl2 = map.createLayer("cleanup2", ['citytiles', 'schoolassets', 'ground', 'chars'], 0, 0);
 
         coll.setCollisionByExclusion([-1])
         cl.setCollisionByProperty({ col: true })
@@ -122,27 +131,41 @@ const main = {
             width,
             height
         }, true)
+       /* const emotes = this.emotes = this.add.particles('heartEmote').createEmitter( {
 
+            gravityY: 100,
+            scale: 1,
+            emitting: false
+        })
+        //emotes.setDepth(14)*/
         // fill it with black
         hKey = this.input.keyboard.addKey('H')
-        sKey = this.input.keyboard.addKey(this.input.keyboard.keyCodes.SPACE)
+        sKey = this.input.keyboard.addKey(32)
         rt.setDepth(14)
         player.setDepth(13)
         console.log(map)
     },
     onFrame: function (t, delta) {
+        var pointer = this.input.mousePointer.positionToCamera(this.cameras.main)
         if (win != 0) { return }
         if (hKey.isDown) hiding = !hiding
-        //console.log(getTime())
-        if (sKey.getDuration() ==emo){
-        var closest=map.findObject("data", (obj) => {
-            if(obj.name === "h"&&getDistance(player.x,player.y,obj.x,obj.y)<=2){
-                return obj
+
+        if (sKey.getDuration() > emo) {
+            //console.log(sKey)
+            var tile = this.cl2.getTileAt(...[map.worldToTileX(pointer.x), map.worldToTileY(pointer.y)])
+            console.log(tile)
+            if (tile != null) {
+                this.sound.add('child').play()
+                emo = rand(5000, 1000)
+                map.removeTile(tile)
+                children++
+                var wrd='Children';
+                if(7-children==1)wrd='Child'
+                document.getElementById('hr').innerText = `${7-children} ${wrd} Left`
+                document.getElementById('rtime').innerText=``
+                chldcol=true
             }
-        });
-        emo = rand(5000, 1000)
-        map.removeTile(closest)
-        children++
+
         }
         document.getElementsByClassName('text')[0].innerText = getTime()
 
@@ -174,21 +197,21 @@ const main = {
         this.rt.clear();
         this.rt.fill(0x000000, 1)
         this.rt.setTint(0x0a2948)
-
+        if(preattack){
+            preattack=false
+            this.sound.add('bully',{volume:.5}).play()
+        }
         if (!hiding) this.rt.erase('mask', this.player.x - 53, this.player.y - 53)
         if (!hiding && attack) { win = -1; loss() }
         if (hour[0] == 6 && children == 7) { win = 1; winGame() }
         if (hour[0] == 6 && hour[1] == 5 && win == 0) { win = -1; loss() }
+        if(hour[1]>55||hour[1]<6){
         document.getElementById('hr').innerText = `${getTime().split(':')[0]} AM`
         document.getElementById('rtime').innerText = `${Math.abs(6 - hour[0])} hours remaining`
+        }
 
 
 
-        /*const emotes=this.emotes=this.add.particles(player.x,player.y,{
-            frame:['emotes'],
-            gravityY: 200,
-            emitting: false
-        })*/
     }
 }
 function getTime() {
@@ -200,15 +223,15 @@ function getTime() {
         res = res + '0'
     }
 
-    if (hour[1] == 0) { document.getElementsByClassName('center')[0].style.display = 'block' }
-    if (hour[1] == 5) { document.getElementsByClassName('center')[0].style.display = 'none' }
+    if (hour[1] == 0||chldcol) { document.getElementsByClassName('center')[0].style.display = 'block';chldcol=false;fadeTime=hour[1]+5 }
+    if (hour[1] == fadeTime) { document.getElementsByClassName('center')[0].style.display = 'none' }
     return res + hour[1]
 }
 function getDistance(x1, y1, x2, y2) {
     return Math.hypot(x2 - x1, y2 - y1)
 }
 setInterval(() => {
-    if ((hour[0] >= 6 && hour[1] >= 9 && hour[0] < 12) || (hour[1] > hattack + 4 && win == -1)) {
+    if ((hour[0] >= 6 && hour[1] >= 9 && hour[0] < 12) || (hour[1] > hattack + 3 && win == -1)) {
         document.getElementsByTagName('canvas')[0].remove()
         document.getElementsByClassName('game')[0].remove()
         return
@@ -219,6 +242,9 @@ setInterval(() => {
         hattack = rand(50, 10)
     } else {
         hour[1]++
+    }
+    if (hour[1] == hattack-5) {
+        preattack=true
     }
     if (hour[1] == hattack) {
         attack = true
